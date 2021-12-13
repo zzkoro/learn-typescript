c5c28282353c18e7eb0740c1aaafc4ca1bec5999
junemp <junemp@gmail.com>
2021-12-14 07:10:13 +0900

    수정사항 업로드

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,44 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {Object} Address
+ * @property {string} street
+ * @property {string} city 
+ */
+
+/**
+ * @typedef {Object} User
+ * @property {string} name
+ * @property {string} email 
+ * @property {Address} address
+ */
+
+/**
+ * 
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.data.name;
+  response.data.address.zipcode;
+});
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street
     })
     .catch(function (error) {
       console.log(error);
